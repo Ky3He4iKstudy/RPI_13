d169adeed15418493f5399a989cc89a76e6c7727
ky3he4ik <misha10311@yandex.ru>
2022-04-30 09:51:01 +0300

    Update

--- pr5_3/app/src/main/java/com/example/preferencesscreenexample/SettingsActivity.kt
+++ pr5_3/app/src/main/java/com/example/preferencesscreenexample/SettingsActivity.kt
@@ -3,7 +3,6 @@ package com.example.preferencesscreenexample
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,26 +10,20 @@ import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 
 class SettingsActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
+        if (savedInstanceState == null)
+            supportFragmentManager.beginTransaction().replace(R.id.settings, SettingsFragment())
                 .commit()
-        }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         sharedPreferences.registerOnSharedPreferenceChangeListener(this)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) =
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
-
-        }
     }
 
     override fun onDestroy() {
@@ -39,17 +32,17 @@ class SettingsActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferen
         super.onDestroy()
     }
 
-
     override fun onResume() {
         super.onResume()
-        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .registerOnSharedPreferenceChangeListener(this)
     }
 
     override fun onPause() {
         super.onPause()
-        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .unregisterOnSharedPreferenceChangeListener(this)
     }
-
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
         when (key) {
@@ -57,22 +50,17 @@ class SettingsActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferen
                 val test = sharedPreferences!!.getBoolean("example_switch", false)
                 Toast.makeText(this, test.toString(), Toast.LENGTH_SHORT).show()
             }
-            "list" ->{
-                val test = sharedPreferences!!.getString("list", "")
-                val settingsView = findViewById<View>(R.id.settings)
-                if(test != null){
-                    if(test == "1"){
-                        settingsView.setBackgroundColor(Color.RED)
+            "list" -> {
+                val test = sharedPreferences?.getString("list", null) ?: return
+                findViewById<View>(R.id.settings).setBackgroundColor(
+                    when (test) {
+                        "1" -> Color.RED
+                        "2" -> Color.GREEN
+                        "3" -> Color.BLUE
+                        else -> Color.WHITE
                     }
-                    if(test == "2"){
-                        settingsView.setBackgroundColor(Color.GREEN)
-                    }
-                    if(test == "3"){
-                        settingsView.setBackgroundColor(Color.BLUE)
-                    }
-                }
+                )
             }
-
         }
     }
 }
